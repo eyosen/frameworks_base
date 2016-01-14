d6fe10a712d299dc043b8a6a1e693180e87b828c
beanstown106 <nbenis106@gmail.com>
2016-01-14 05:56:50 -0500

    [1/2] base: Fingerprint authentication vibration
    had a few requests for this, as most devices only vibrate
    when fingerprint authentication is unsucesful so made it configurable
    
    Change-Id: Ie1c0f5276e034b58b4f148d661f5c20e667f6866

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5535,6 +5535,13 @@ public final class Settings {
          */
         public static final String STATUS_BAR_BRIGHTNESS_CONTROL = "status_bar_brightness_control";
 
+        /**
+         * whether to enable or disable vibration on succesful fingerprint auth
+         *
+         * @hide
+         */
+        public static final String FINGERPRINT_SUCCESS_VIB = "fingerprint_success_vib";
+
         /***************************
          * AICP System Settings end
          ***************************/
@@ -5720,6 +5727,7 @@ public final class Settings {
             PRIVATE_SETTINGS.add(DOZE_ON_CHARGE);
             PRIVATE_SETTINGS.add(DOZE_ON_CHARGE_NOW);
             PRIVATE_SETTINGS.add(STATUS_BAR_BRIGHTNESS_CONTROL);
+            PRIVATE_SETTINGS.add(FINGERPRINT_SUCCESS_VIB);
         }
 
         /**
