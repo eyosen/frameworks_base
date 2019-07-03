70a8741ecc6c14dd9b59d24c47440d03f5d82266
Danny Baumann <dannybaumann@web.de>
2019-07-03 15:12:22 +0200

    SystemUI: Add VPN tile
    Adapted to 12 and VpnManager by @eyosen
    
    Author: Danny Baumann <dannybaumann@web.de>
    Date:   Wed Jul 3 15:12:22 2019 +0200
    
        SystemUI: Add VPN tile
    
        Allows quickly connecting/disconnecting to/from VPNs.
    
    Author: Bruno Martins <bgcngm@gmail.com>
    Date:   Wed Oct 7 22:51:51 2020 +0100
    Edit    Update to Android 11 APIs
    
    Co-Authored-By: Ali B. <abittin@gmail.com>
    Co-Authored-By: George Zacharia <george.zcharia@gmail.com>
    Change-Id: Iaa9413bf1071805be8764d92bc5e2dd3131fdaed

--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
@@ -69,6 +69,7 @@ import com.android.systemui.qs.tiles.UiModeNightTile;
 import com.android.systemui.qs.tiles.UsbTetherTile;
 import com.android.systemui.qs.tiles.UserTile;
 import com.android.systemui.qs.tiles.VolumeTile;
+import com.android.systemui.qs.tiles.VpnTile;
 import com.android.systemui.qs.tiles.WeatherTile;
 import com.android.systemui.qs.tiles.WifiTile;
 import com.android.systemui.qs.tiles.WorkModeTile;
@@ -131,6 +132,7 @@ public class QSFactoryImpl implements QSFactory {
     private final Provider<SoundSearchTile> mSoundSearchTileProvider;
     private final Provider<ScreenshotTile> mScreenshotTileProvider;
     private final Provider<WeatherTile> mWeatherTileProvider;
+    private final Provider<VpnTile> mVpnTileProvider;
 
     private final Lazy<QSHost> mQsHostLazy;
     private final Provider<CustomTile.Builder> mCustomTileBuilderProvider;
@@ -185,7 +187,8 @@ public class QSFactoryImpl implements QSFactory {
             Provider<AicpExtrasTile> aicpExtrasTileProvider,
             Provider<SoundSearchTile> soundSearchTileProvider,
             Provider<ScreenshotTile> screenshotTileProvider,
-            Provider<WeatherTile> weatherTileProvider) {
+            Provider<WeatherTile> weatherTileProvider,
+            Provider<VpnTile> vpnTileProvider) {
         mQsHostLazy = qsHostLazy;
         mCustomTileBuilderProvider = customTileBuilderProvider;
 
@@ -236,6 +239,7 @@ public class QSFactoryImpl implements QSFactory {
         mSoundSearchTileProvider = soundSearchTileProvider;
         mScreenshotTileProvider = screenshotTileProvider;
         mWeatherTileProvider = weatherTileProvider;
+        mVpnTileProvider = vpnTileProvider;
     }
 
     public QSTile createTile(String tileSpec) {
@@ -343,6 +347,8 @@ public class QSFactoryImpl implements QSFactory {
                 return mScreenshotTileProvider.get();
             case "weather":
                 return mWeatherTileProvider.get();
+            case "vpn":
+                return mVpnTileProvider.get();
         }
 
         // Custom tiles
