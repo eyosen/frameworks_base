3b40e15dc9ce7b5ea4891e5443598bd293c7c30e
Jason Edson <jaysonedson@gmail.com>
2021-01-07 16:22:43 -0800

    SystemUI: Add flipendo to Battery Saver QSTile
    Add Extreme Battery Saver(Flipendo) to the Battery Saver tile.
    
    Change-Id: I5942ff58d67ce30d3883261b8c2d6dbd53e74585
    Signed-off-by: Jason Edson <jaysonedson@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/tiles/BatterySaverTile.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/BatterySaverTile.java
@@ -59,6 +59,7 @@ public class BatterySaverTile extends QSTileImpl<BooleanState> implements
     private boolean mCharging;
     private boolean mDetailShown;
     private boolean mPluggedIn;
+    private boolean mExtreme;
 
     private Icon mIcon = ResourceIcon.get(com.android.internal.R.drawable.ic_qs_battery_saver);
 
@@ -157,7 +158,9 @@ public class BatterySaverTile extends QSTileImpl<BooleanState> implements
                 state.label = mLevel + "%";
             }
         }
-        state.secondaryLabel = "";
+        state.secondaryLabel = state.state == Tile.STATE_ACTIVE &&
+                mExtreme ? mContext.getString(R.string.extreme_battery_saver_text) : "";
+        state.stateDescription = state.secondaryLabel;
         state.contentDescription = state.label;
         state.value = mPowerSave;
         state.expandedAccessibilityClassName = Switch.class.getName();
@@ -300,5 +303,13 @@ public class BatterySaverTile extends QSTileImpl<BooleanState> implements
                 postBindView();
             }
         };
+    }
+
+    @Override
+    public void onExtremeBatterySaverChanged(boolean isExtreme) {
+        if (mExtreme != isExtreme) {
+            mExtreme = isExtreme;
+            refreshState();
+        }
     }
 }
