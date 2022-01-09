f282bd763b9eb80495351b074b4d5a9eed6abd0a
maxwen <max.weninger@gmail.com>
2022-01-09 17:39:28 +0100

    [1/2] SystemUI: qs hide label setting
    Change-Id: Icf697b7251aed37a77630cdca906a9912f1d8ea2

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5475,6 +5475,11 @@ public final class Settings {
          */
         public static final String QS_TILE_VERTICAL_LAYOUT = "qs_tile_vertical_layout";
 
+        /**
+         * @hide
+         */
+        public static final String QS_TILE_LABEL_HIDE = "qs_tile_label_hide";
+
         /***************************
          * AICP System Settings end
          ***************************/
@@ -5649,6 +5654,7 @@ public final class Settings {
             PRIVATE_SETTINGS.add(QS_LAYOUT_COLUMNS_LANDSCAPE);
             PRIVATE_SETTINGS.add(QS_LAYOUT_COLUMNS);
             PRIVATE_SETTINGS.add(QS_TILE_VERTICAL_LAYOUT);
+            PRIVATE_SETTINGS.add(QS_TILE_LABEL_HIDE);
         }
 
         /**
