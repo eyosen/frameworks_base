a236916c75146fedf0369db81d7a20c4af55dc06
Pranav Vashi <neobuddy89@gmail.com>
2022-01-17 21:20:04 +0530

    Allow toggling camera/mic privacy indicator [1/2]
    Change-Id: Id8cb13a553c630746925ec0c2c7459312e58de30

--- packages/SettingsProvider/src/android/provider/settings/backup/SecureSettings.java
+++ packages/SettingsProvider/src/android/provider/settings/backup/SecureSettings.java
@@ -228,5 +228,6 @@ public class SecureSettings {
         Settings.Secure.PULSE_SMOOTHING_ENABLED,
         Settings.Secure.PULSE_SOLID_UNITS_ROUNDED,
         Settings.Secure.ENABLE_LOCATION_PRIVACY_INDICATOR,
+        Settings.Secure.ENABLE_CAMERA_PRIVACY_INDICATOR,
     };
 }
