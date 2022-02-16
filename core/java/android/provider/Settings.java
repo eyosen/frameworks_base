4abe98852bca6053283e482970166cb9c7d9bf30
Ali B <abittin@gmail.com>
2022-02-16 23:22:41 +0300

    base: core: add missing activity blacklist setting
    Change-Id: I12a420300dc9a7a99d9139b249a751fe667231ed

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6282,6 +6282,12 @@ public final class Settings {
         public static final String VOWIFI_ICON = "vowifi_icon";
 
         /**
+         * VoWiFi icon style
+         * @hide
+         */
+        public static final String VOWIFI_ICON_STYLE = "vowifi_icon_style";
+
+        /**
          * Whether the phone ringtone should be played in an increasing manner
          * @hide
          */
@@ -6299,6 +6305,12 @@ public final class Settings {
          */
         public static final String INCREASING_RING_RAMP_UP_TIME = "increasing_ring_ramp_up_time";
 
+        /**
+         * Smart Pixels pattern
+         * @hide
+         */
+        public static final String CHOOSER_ACTIVITY_BLACKLIST = "chooser_activity_blacklist";
+
         /***************************
          * AICP System Settings end
          ***************************/
@@ -6309,12 +6321,6 @@ public final class Settings {
          * it to PRIVATE_SETTINGS below. Also add a validator that can validate
          * the setting value. See an example above.
          */
-
-        /**
-         * VoWiFi icon style
-         * @hide
-         */
-        public static final String VOWIFI_ICON_STYLE = "vowifi_icon_style";
 
         /**
          * Keys we no longer back up under the current schema, but want to continue to
@@ -6596,6 +6602,7 @@ public final class Settings {
             PRIVATE_SETTINGS.add(INCREASING_RING);
             PRIVATE_SETTINGS.add(INCREASING_RING_RAMP_UP_TIME);
             PRIVATE_SETTINGS.add(INCREASING_RING_START_VOLUME);
+            PRIVATE_SETTINGS.add(CHOOSER_ACTIVITY_BLACKLIST);
         }
 
         /**
