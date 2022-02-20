141a51d07966e8ea7429afa4d0375b430d2d99d3
Ali B <abittin@gmail.com>
2022-02-20 22:28:45 +0300

    base: fix non-system app causing app fc for volume change
    E AndroidRuntime: FATAL EXCEPTION: main
    E AndroidRuntime: Process: com.google.android.youtube, PID: 9243
    E AndroidRuntime: java.lang.SecurityException: Settings key: <swap_volume_buttons> is not readable. From S+, settings keys annotated with @hide are restricted to system_server and system apps only, unless they are annotated with @Readable.
    
    Change-Id: Ia0a9bd41a6c42a5393f76644c3f702b6459c084a

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5856,6 +5856,7 @@ public final class Settings {
          * Change volume up and down handlign based on rotation
          * @hide
          */
+        @Readable
         public static final String SWAP_VOLUME_BUTTONS = "swap_volume_buttons";
 
         /**
